95294aae0b2403ee1ad40573b3369e5f8319e83d
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2021-12-18 01:28:05 +0000

    Main menu finished, timer implemented
    You can now go / un-go fullscreen, change screen resolution, and increase / decrease volume. Timer also fixed so it doesn't show ':60' when going over a new minute

---
+++ Oakwell-Stronghold/Assets/Scripts/Random/Timer.cs
@@ -0,0 +1,45 @@
+﻿#region 'Using' information
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+#endregion
+
+public class Timer : MonoBehaviour
+{
+    static Timer instance;
+    float countdown = 302.0f;
+    static bool active;
+    public TMP_Text timerUI;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        if (countdown > 0) // Gradually counts down until reaching 0.
+        countdown -= Time.deltaTime;
+
+        int displayTimer = Mathf.CeilToInt(countdown);
+
+        string minutes = ((int)displayTimer / 60).ToString();
+        string seconds = (displayTimer % 60).ToString("00");
+
+        double b = System.Math.Round(countdown, 2);
+
+        timerUI.text = minutes + ":" + seconds;
+
+        if (countdown < 0)
+        {
+            StartCoroutine("LoadNewScene"); // Loads the main menu when timer hits 0
+        }
+    }
+
+    void LoadNewScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Oakwell-Stronghold/Assets/SettingsMenu.cs
@@ -0,0 +1,59 @@
+﻿#region 'Using' information
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+#endregion
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    Resolution[] resolutions;
+
+    List<string> options = new List<string>();
+
+    public TMP_Dropdown resolutionDropdown;
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i] + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
