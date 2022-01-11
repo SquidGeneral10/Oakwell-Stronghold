28860bdf2908f9efe11a7762cc43f21b3ccee4a3
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2022-01-11 10:32:15 +0000

    Level 2 and 3 Complete

--- Oakwell-Stronghold/Assets/Scripts/Random/Timer2.cs
+++ Oakwell-Stronghold/Assets/Scripts/Random/Timer2.cs
@@ -10,7 +10,7 @@ public class Timer2 : MonoBehaviour
 {
     static Timer2 instance;
 
-    public float countdown = 180.0f; // Every level will count down from 3 minutes. CHANGE IN EDITOR.
+    public float countdown = 120.0f; // Every level will count down from 2 minutes. CHANGE IN EDITOR, IF YOU MUST.
     static bool active;
     public float transitionTime = 1f;
     public TMP_Text timerUI;
@@ -56,12 +56,12 @@ public class Timer2 : MonoBehaviour
 
         timerUI.text = minutes + ":" + seconds;
 
-        if (countdown >= 120) // player still has 3 stars til a minute's passed
+        if (countdown >= 90) // player still has 3 stars til a minute's passed
         {
             DataManager.Instance.Level2Stars = 3;
         }
 
-        if (countdown <= 119f)
+        if (countdown <= 89f)
         {
             purpleStarA.SetActive(false); // Removes the purple star that's always visible
             purpleStarB.SetActive(false); // Removes the purple star on the LevelComplete UI         
